106cc1be0c3f6582b34e9829be6a620446730dcb
jordanlikescoding <156983998+jordanlikescoding@users.noreply.github.com>
2025-04-09 15:50:34 -0500

    Update hardware_interface_adafruit_PWM_hat.cpp
    wrote in return statements for exporting state and command interfaces and included read and write function with no code inside

--- hardware_interface_adafruit_PWM_hat.cpp
+++ hardware_interface_adafruit_PWM_hat.cpp
@@ -6,7 +6,10 @@
 //        example when system moves from intializtion to configuration
 //-----------NOTES------------------
 //  the lifecycle which includes state transitions and callbacks are important because the purpose of using ros control is to control when to establish connections, communication, power, etc 
-//---------------------------------------------------
+//----------------Next steps---------------------------
+//  we will need to write xml file and in the urd include the hardware interface in the ros2control tags
+//  we need to build the package and make sure things are working correctly
+//  in the video, he writes code to have serial communciation with the arduno board, in our case we will nned to write code for adafruit
 
 
 namespace custom_hardware{
@@ -72,10 +75,40 @@ namespace custom_hardware{
 
     //callback function that exectues when state trasition occurs when on_configure is done and sucessful
     hardware_interface::CallbackReturn CustomHardwareInterfaceAdafruitPWMHat::on_activate(const rclcpp_lifecycle::State & previous_state){
-
+        //this function is used to actiave certain hardware when an ecent happens, this function can be used with setting up the camera or something else
+        return hardware_interface::CallbackReturn::SUCESS;
     }
 
     hardware_interface::CallbackReturn CustomHardwareInterfaceAdafruitPWMHat::on_deactivate(const rclcpp_lifecycle::State & previous_state){
+        //this is the opposite action for on_activate, it will just stop the activity that was triggered
+        return hardware_interface::CallbackReturn::SUCESS;
+    }
+    //function basically as it says exports state interfaces, where the state interfaces is states of each joint and other information regarinding to that
+    std::vector<hardware_interface::StateInterface> CustomHardwareInterfaceAdafruitPWMHat::export_state_interfaces(){
+        std::cvector<hardware_interface::StateInterface> state_interfaces;
+        for(uint i = 0; i < info_.joints.size(); i++){
+            state_interfaces.emplace_back(hardware_interface::StateInterface(into_.joints[i].name, hardware_interface::HW_IF_POSITION, &hw_states_position_[i]));
+            state_interfaces.emplace_back(hardware_interface::StateInterface(info_.joints[i].name, hardware_interface::HW_IF_VELOCITY, &hw_states_velocity_[i]));
+        }
+        return state_interfaces;
+    }
 
+    //function basically as it says exports command interfaces
+    std::vector<hardware_interface::CommandInterface> CustomHardwareInterfaceAdafruitPWMHat::export_command_interfaces(){
+        std::vector<hardware_interface::CommandInterface> command_interfaces;
+        for(uint i = 0; i < info_.joints.size(); i++){
+            command_interfaces.emplace_back(hardware_interface::CommandInterface(info_.joints[i].name, hardware_interface::HW_IF_POSITION, &hw_commands_[i]));
+        }
+        return command_interfaces;
+    }
+
+    //the function will read position of each servo motor, we will probably have to write a seperate file using adafruit library 
+    hardware_interface::return_type CustomHardwareInterfaceAdafruitPWMHat::read(const rclcpp::Time & time, const rclcpp::Duration & period){
+        return hardware_interface::return_type::OK;
+    }
+
+    //same as above but will be writing to the motors
+    hardware_interface::return_type CustomHardwareInterfaceAdafruitPWMHat::write(const rclcpp::Time & time, const rclcpp::Duration & period){
+        return hardware_interface::return_type::OK;
     }
 }
