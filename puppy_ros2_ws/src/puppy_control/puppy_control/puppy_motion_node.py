52ed68757163ec15663fc6f2564455df7b261f30
Art <b.k.arthur@live.com>
2025-04-22 13:46:37 -0500

    Renamed key_input_node to be run correctly

--- puppy_ros2_ws/src/puppy_control/puppy_control/puppy_motion_node.py
+++ puppy_ros2_ws/src/puppy_control/puppy_control/puppy_motion_node.py
@@ -1,42 +1,7 @@
-# #!/usr/bin/env python3
+# dummy file for motion
 
-# import rclpy
-# from rclpy.node import Node
-# from std_msgs.msg import String
-# import movement
+def main():
+    print("Motion node executed")
 
-# class MotionCommandNode(Node):
-#     def __init__(self):
-#         super().__init__('puppy_motion_node')
-#         self.subscriber = self.create_subscription(
-#             String,
-#             '/puppy_motion_command',
-#             self.command_callback,
-#             10
-#         )
-#         self.get_logger().info('Motion Command Node ready.')
-
-#     def command_callback(self, msg):
-#         command = msg.data.lower()
-#         self.get_logger().info(f'Received command: {command}')
-
-#         if command == 'walk':
-#             movement.trot_forward()
-#         elif command == 'turn_left':
-#             movement.turn_left()
-#         elif command == 'stand':
-#             movement.stand()
-#         else:
-#             self.get_logger().warn(f'Unknown command: {command}')
-
-# def main():
-#     rclpy.init()
-#     node = MotionCommandNode()
-#     rclpy.spin(node)
-#     node.destroy_node()
-#     rclpy.shutdown()
-
-# if __name__ == '__main__':
-#     main()
-        
-        
+if __name__ == "__main__":
+    main()
