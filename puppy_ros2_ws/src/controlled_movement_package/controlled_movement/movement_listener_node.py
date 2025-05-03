aecb093b6b4a539041ae7e9005e0a354305b020f
Euttholb <Traise_1@outlook.com>
2025-05-03 03:19:26 -0500

    changed movement.py

--- puppy_ros2_ws/src/controlled_movement_package/controlled_movement/movement_listener_node.py
+++ puppy_ros2_ws/src/controlled_movement_package/controlled_movement/movement_listener_node.py
@@ -9,6 +9,43 @@ import time
 start_time = time.time()
 time_now = time.time()
 
+# class MovementListener(Node):
+#     def __init__(self):
+#         super().__init__('movement_listener')
+#         self.subscription = self.create_subscription(
+#             Int32MultiArray,
+#             'command_topic',
+#             self.listener_callback,
+#             10
+#         )
+#         self.subscription  # prevent unused variable warning
+#         self.get_logger().info('Movement Listener Node has started.')
+
+#     def listener_callback(self, msg):
+#         # msg.data = [up, down, left, right] (0 or 1 each)
+#         movement_states = msg.data
+#         self.get_logger().info(f"Received movement command: {movement_states}")
+
+#         if movement_states[0]:  # Up key (W)
+#             self.get_logger().info('Walking forward...')
+#             movement.trot_forward(time_now, start_time)
+
+#         elif movement_states[1]:  # Down key (S)
+#             self.get_logger().info('Walking backward...')
+#             movement.walk_back(time_now, start_time)  # Not implemented
+
+#         elif movement_states[2]:  # Left key (A)
+#             self.get_logger().info('Turning left...')
+#             movement.turn_left(time_now, start_time)
+
+#         elif movement_states[3]:  # Right key (D)
+#             self.get_logger().info('Turning right...')
+#             movement.turn_right(time_now, start_time)
+
+#         else:
+#             self.get_logger().info('Standing still.')
+#             movement.stand()
+
 class MovementListener(Node):
     def __init__(self):
         super().__init__('movement_listener')
@@ -18,33 +55,35 @@ class MovementListener(Node):
             self.listener_callback,
             10
         )
-        self.subscription  # prevent unused variable warning
+        self.motion_command = [0, 0, 0, 0]  # store the latest command
+        self.start_time = time.time()
+
+        # Run a timer callback at 50Hz
+        self.timer = self.create_timer(0.02, self.motion_loop)
+
         self.get_logger().info('Movement Listener Node has started.')
 
     def listener_callback(self, msg):
-        # msg.data = [up, down, left, right] (0 or 1 each)
-        movement_states = msg.data
-        self.get_logger().info(f"Received movement command: {movement_states}")
+        self.motion_command = msg.data
 
-        if movement_states[0]:  # Up key (W)
-            self.get_logger().info('Walking forward...')
-            movement.trot_forward(time_now, start_time)
+    def motion_loop(self):
+        time_now = time.time()
 
-        elif movement_states[1]:  # Down key (S)
-            self.get_logger().info('Walking backward...')
-            movement.walk_back(time_now, start_time)  # Not implemented
+        if self.motion_command[0]:  # Forward
+            movement.trot_forward_step(time_now, self.start_time)
 
-        elif movement_states[2]:  # Left key (A)
-            self.get_logger().info('Turning left...')
-            movement.turn_left(time_now, start_time)
+        elif self.motion_command[1]:  # Backward
+            movement.walk_back_step(time_now, self.start_time)
 
-        elif movement_states[3]:  # Right key (D)
-            self.get_logger().info('Turning right...')
-            movement.turn_right(time_now, start_time)
+        elif self.motion_command[2]:  # Left
+            movement.turn_left_step(time_now, self.start_time)
+
+        elif self.motion_command[3]:  # Right
+            movement.turn_right_step(time_now, self.start_time)
 
         else:
-            self.get_logger().info('Standing still.')
             movement.stand()
+
 
 def main(args=None):
     rclpy.init(args=args)
